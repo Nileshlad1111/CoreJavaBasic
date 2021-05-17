bbbbe5ee279c7375d65f638fa1dc373cabf561cb
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-17 08:08:44 +0530

    Added:To for loop examples

---
+++ src/com/forLoop/NumberofOnetoTen.java
@@ -0,0 +1,12 @@
+package com.forLoop;
+
+public class NumberofOnetoTen {
+
+	public static void main(String[] args) {
+		int number =10;
+		for (int index = 0; index >number; index++) {
+			
+			System.out.println(index);
+		}
+	}
+}
---
+++ src/com/forLoop/OneToTenReverseNumber.java
@@ -0,0 +1,12 @@
+package com.forLoop;
+
+public interface OneToTenReverseNumber {
+
+	public static void main(String[] args) {
+		
+		int number=0;
+		for (int i = 10; i>=number; i--) {
+			System.out.println(i);
+		}
+	}
+}
